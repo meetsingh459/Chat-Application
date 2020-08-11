75157c551edaa52c5a82e88349c1f09c7fb121bf
Manmeet Singh <meetsingh459@gmail.com>
2020-08-12 01:34:37 +0530

    Updates

---
+++ precache-manifest.6dab0b49e06f8b9435d13c50ca95bad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d585593c2e9572b27b6b11dd0481fc68",
+    "url": "/Chat-Application/index.html"
+  },
+  {
+    "revision": "3be7d26b32703ed8c679",
+    "url": "/Chat-Application/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bff48f9f9dbcbee60df9",
+    "url": "/Chat-Application/static/css/main.b2038b64.chunk.css"
+  },
+  {
+    "revision": "3be7d26b32703ed8c679",
+    "url": "/Chat-Application/static/js/2.f853a63b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/Chat-Application/static/js/2.f853a63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff48f9f9dbcbee60df9",
+    "url": "/Chat-Application/static/js/main.e23e4114.chunk.js"
+  },
+  {
+    "revision": "02cec59cafbad8dd4e10",
+    "url": "/Chat-Application/static/js/runtime-main.26fc4d4e.js"
+  }
+]);
